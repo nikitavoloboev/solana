eb90ee39a13b1f70d33f59548db3c2451579c82d
Nikita <github@nikiv.dev>
2024-11-05 14:59:37 +0100

    .

--- web/app/routes/__root.tsx
+++ web/app/routes/__root.tsx
@@ -1,36 +1,24 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
 import {
-  Link,
-  Outlet,
-  ScrollRestoration,
-  createRootRouteWithContext,
-} from "@tanstack/react-router"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react"
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets"
+import { clusterApiUrl } from "@solana/web3.js"
+import type { QueryClient } from "@tanstack/react-query"
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router"
 import { Body, Head, Html, Meta, Scripts } from "@tanstack/start"
 import * as React from "react"
-import type { QueryClient } from "@tanstack/react-query"
 import { DefaultCatchBoundary } from "~/components/DefaultCatchBoundary"
 import { NotFound } from "~/components/NotFound"
 // @ts-expect-error
 import appCss from "~/styles/app.css?url"
 import { seo } from "~/utils/seo"
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react"
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  TorusWalletAdapter,
-  UnsafeBurnerWalletAdapter,
-} from "@solana/wallet-adapter-wallets"
-import {
-  WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui"
-import { clusterApiUrl } from "@solana/web3.js"
 
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css"
